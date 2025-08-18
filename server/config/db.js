45df541124fac3819f90184c1127b3a8e2148a10
unknown <Yashika Chauhan>
2025-08-18 20:17:26 +0530

    8:22

--- server/config/db.js
+++ server/config/db.js
@@ -1,7 +1,13 @@
 import mongoose from "mongoose";
 
-
 export const connectDB = async () => {
-  await mongoose.connect(`${process.env.MONGO_URI}/fmsSystem` );
+  try {
+    await mongoose.connect(`${process.env.MONGO_URI}/fmsSystem`, {
+  
+    });
     console.log("MongoDB Connected");
+  } catch (error) {
+    console.error("MongoDB connection failed:", error);
+    process.exit(1); // Exit process if DB connection fails
+  }
 };
